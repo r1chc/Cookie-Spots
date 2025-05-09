d4da3307a238031d22516f944a6cdc0d5e3fbd6d
r1chc1 <richardcibu@yahoo.com>
2025-05-09 14:53:34 -0400

    fix: simplify Vite config and add Netlify config

--- vite.config.js
+++ vite.config.js
@@ -3,47 +3,37 @@ import react from '@vitejs/plugin-react'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig(({ command }) => {
-  const config = {
-    plugins: [react()],
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, './src'),
-      },
+export default defineConfig({
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    build: {
-      outDir: 'dist',
-      sourcemap: command === 'serve',
-      minify: command === 'build',
-      rollupOptions: {
-        input: {
-          main: path.resolve(__dirname, 'index.html'),
-        },
-        output: {
-          manualChunks: {
-            vendor: ['react', 'react-dom', 'react-router-dom'],
-            maps: ['@googlemaps/js-api-loader', '@googlemaps/markerclusterer', 'google-map-react'],
-            ui: ['@heroicons/react', 'react-slick', 'slick-carousel'],
-          },
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+    minify: true,
+    rollupOptions: {
+      input: path.resolve(__dirname, 'index.html'),
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          maps: ['@googlemaps/js-api-loader', '@googlemaps/markerclusterer', 'google-map-react'],
+          ui: ['@heroicons/react', 'react-slick', 'slick-carousel'],
         },
       },
     },
-  }
-
-  if (command === 'serve') {
-    // Development configuration
-    config.server = {
-      port: 5173,
-      open: true,
-      proxy: {
-        '/api': {
-          target: 'http://localhost:5002',
-          changeOrigin: true,
-          secure: false
-        }
+  },
+  server: {
+    port: 5173,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:5002',
+        changeOrigin: true,
+        secure: false
       }
     }
   }
-
-  return config
 })
