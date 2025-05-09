4ac1e41c669d63e2f106e716a63ae598f7512f97
r1chc1 <richardcibu@yahoo.com>
2025-05-09 15:22:01 -0400

    fix: downgrade vite to v5 and simplify config

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: '.',
   plugins: [react()],
   resolve: {
     alias: {
@@ -16,9 +15,6 @@ export default defineConfig({
     sourcemap: false,
     minify: true,
     rollupOptions: {
-      input: {
-        app: path.resolve(__dirname, 'index.html'),
-      },
       output: {
         manualChunks: {
           vendor: ['react', 'react-dom', 'react-router-dom'],
