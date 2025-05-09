c113216d9e5d4132817ba7db2316f2f598afbb97
r1chc1 <richardcibu@yahoo.com>
2025-05-09 19:36:36 -0400

    fix: simplify vite config to resolve build issues

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import path from 'path'
-import { fileURLToPath } from 'url'
-
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  root: process.cwd(),
   plugins: [react()],
   resolve: {
     alias: {
@@ -18,19 +13,7 @@ export default defineConfig({
   build: {
     outDir: 'dist',
     sourcemap: false,
-    minify: true,
-    rollupOptions: {
-      input: {
-        main: path.resolve(process.cwd(), 'index.html'),
-      },
-      output: {
-        manualChunks: {
-          vendor: ['react', 'react-dom', 'react-router-dom'],
-          maps: ['@googlemaps/js-api-loader', '@googlemaps/markerclusterer', 'google-map-react'],
-          ui: ['@heroicons/react', 'react-slick', 'slick-carousel'],
-        },
-      },
-    },
+    minify: true
   },
   server: {
     port: 5173,
